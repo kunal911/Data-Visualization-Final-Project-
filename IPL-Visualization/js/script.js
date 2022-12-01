f71ef7cc833032ce4469b2e6af3e99d92b37bee7
dhruvilshah25 <dhruvil.d.shah25@gmail.com>
2022-12-01 14:07:19 -0700

    ID obtained implement grouped bar

--- IPL-Visualization/js/script.js
+++ IPL-Visualization/js/script.js
@@ -3,7 +3,8 @@
 async function loadData(){
     const matchData = await d3.csv('Data/IPL_Matches_2022.csv');
     const perBallData = await d3.csv('Data/IPL_Ball_by_Ball_2022.csv');
-    return {matchData,perBallData};
+    const rpodata = await d3.csv('Data/RPO_out.csv')
+    return {matchData,perBallData,rpodata};
 }
 
 
@@ -12,6 +13,7 @@ loadData().then(loadedData=>{
     console.log("Data Imported");
     const perBallData = loadedData.perBallData;
     const matchData = loadedData.matchData;
+    const rpodata = loadedData.rpodata;
     header = new matchSelection(matchData,perBallData);
 
 });
