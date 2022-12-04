fac7261dd1bce3ccb3cf60e8d4a249fc724a8010
kunal911 <52748248+kunal911@users.noreply.github.com>
2022-12-03 20:01:44 -0700

    Ranking graph

--- IPL-Visualization/js/script.js
+++ IPL-Visualization/js/script.js
@@ -4,7 +4,8 @@ async function loadData(){
     const matchData = await d3.csv('Data/IPL_Matches_2022.csv');
     const perBallData = await d3.csv('Data/IPL_Ball_by_Ball_2022.csv');
     const rpodata = await d3.csv('Data/RPO_out.csv')
-    return {matchData,perBallData,rpodata};
+    const rankings = await d3.csv("Data/IPL_2008_2022_WINNINGS.csv")
+    return {matchData,perBallData,rpodata,rankings};
 }
 
 
@@ -14,6 +15,10 @@ loadData().then(loadedData=>{
     const perBallData = loadedData.perBallData;
     const matchData = loadedData.matchData;
     const rpodata = loadedData.rpodata;
-    header = new matchSelection(matchData,perBallData);
+    const rankings = loadedData.rankings;
+    header = new matchSelection(matchData,perBallData,rankings);
+
+    //ranking = new Ranking(rankings);
+    //ranking.drawRankingGraph();
 
 });
